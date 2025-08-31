5ef7d47ed42cd4ea0cdc1a779eee607dd3425e82
Jorge Borja <71192472+unfresh25@users.noreply.github.com>
2025-08-30 20:23:40 -0500

    Actualizar TalentShowContext.tsx

--- src/contexts/TalentShowContext.tsx
+++ src/contexts/TalentShowContext.tsx
@@ -30,11 +30,11 @@ const initialParticipants: Participant[] = [
   { id: 2, name: 'María Buzón', talent: 'TED Talk: Exportando emociones: Lo que Europa olvidó incluir en el tratado Schengen', score: 0, hasPerformed: false },
   { id: 3, name: 'Humberto Llinás', talent: 'La Guitarra de Llinás', score: 0, hasPerformed: false },
   { id: 4, name: 'Nicolás Magia', talent: 'Abra Jajabra', score: 0, hasPerformed: false },
-  { id: 5, name: 'Joshua Romero', talent: 'Un adolescente invisible', score: 0, hasPerformed: false },
-  { id: 6, name: 'Laura Noriega', talent: 'Paciencia ', score: 0, hasPerformed: false },
-  { id: 7, name: 'Leoximar', talent: 'Talk and Eat Show', score: 0, hasPerformed: false },
-  { id: 8, name: 'Chris Miu', talent: 'El miauravilloso show de miu', score: 0, hasPerformed: false },
-  { id: 9, name: 'María Angelica Choles', talent: 'Skincare', score: 0, hasPerformed: false },
+  { id: 5, name: 'Laura Noriega', talent: 'Paciencia ', score: 0, hasPerformed: false },
+  { id: 6, name: 'Leoximar', talent: 'Talk and Eat Show', score: 0, hasPerformed: false },
+  { id: 7, name: 'Chris Miu', talent: 'El miauravilloso show de miu', score: 0, hasPerformed: false },
+  { id: 8, name: 'María Angelica Choles', talent: 'Skincare', score: 0, hasPerformed: false },
+  { id: 9, name: 'Lolo', talent: 'Wozy', score: 0, hasPerformed: false },
 ];
 
 export function TalentShowProvider({ children }: { children: React.ReactNode }) {
